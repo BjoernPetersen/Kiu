547089755f79147b8921d8293c5780a714701275
Björn Petersen <pheasn@gmail.com>
2017-11-21 13:14:54 +0100

    Use fragment_suggest in SuggestFragment

--- app/src/main/java/com/github/bjoernpetersen/q/ui/fragments/SuggestFragment.kt
+++ app/src/main/java/com/github/bjoernpetersen/q/ui/fragments/SuggestFragment.kt
@@ -37,7 +37,7 @@ class SuggestFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
       savedInstanceState: Bundle?): View? {
     // Inflate the layout for this fragment
-    return inflater!!.inflate(R.layout.fragment_search, container, false)
+    return inflater!!.inflate(R.layout.fragment_suggest, container, false)
   }
 
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
