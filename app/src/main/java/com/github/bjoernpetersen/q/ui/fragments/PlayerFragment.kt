0de1263ecf4ee7f2af09e69802d7b55ef19615a1
Björn Petersen <pheasn@gmail.com>
2017-11-23 17:43:54 +0100

    Add sensible swipe feedback

--- app/src/main/java/com/github/bjoernpetersen/q/ui/fragments/PlayerFragment.kt
+++ app/src/main/java/com/github/bjoernpetersen/q/ui/fragments/PlayerFragment.kt
@@ -1,19 +1,16 @@
 package com.github.bjoernpetersen.q.ui.fragments
 
 
-import android.content.Context
 import android.os.Bundle
 import android.os.Looper
 import android.support.v4.app.Fragment
-import android.support.v4.view.GestureDetectorCompat
 import android.util.Log
-import android.view.GestureDetector.OnGestureListener
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.Toast
+import com.chauthai.swipereveallayout.SwipeRevealLayout
+import com.github.bjoernpetersen.jmusicbot.client.ApiException
 import com.github.bjoernpetersen.jmusicbot.client.model.PlayerState
 import com.github.bjoernpetersen.q.R
 import com.github.bjoernpetersen.q.api.Auth
@@ -21,6 +18,7 @@ import com.github.bjoernpetersen.q.api.Connection
 import com.github.bjoernpetersen.q.api.Permission
 import com.github.bjoernpetersen.q.tag
 import com.github.bjoernpetersen.q.ui.ObserverUser
+import com.github.bjoernpetersen.q.ui.isVisible
 import com.squareup.picasso.Picasso
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -32,9 +30,6 @@ import java.lang.ref.WeakReference
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
-
-private const val SWIPE_THRESHOLD = 100
-private const val SWIPE_VELOCITY_THRESHOLD = 100
 
 class PlayerFragment : Fragment(), ObserverUser {
 
@@ -50,18 +45,66 @@ class PlayerFragment : Fragment(), ObserverUser {
     return inflater!!.inflate(R.layout.fragment_player, container, false)
   }
 
+  override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    root.setSwipeListener(object : SwipeRevealLayout.SimpleSwipeListener() {
+      /**
+       * Called when the main view becomes completely opened.
+       */
+      override fun onOpened(view: SwipeRevealLayout) {
+        skip_text.isVisible = false
+        skip_progress.isVisible = true
+        skip_progress.animate()
+
+        view.open(true)
+        view.setLockDrag(true)
+
+        Single.fromCallable { Auth.apiKey }
+            .subscribeOn(Schedulers.io())
+            .map { it.raw }
+            .map { Connection.nextSong(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+              updatedState(it)
+              close(view)
+            }, {
+              Log.d(this@PlayerFragment.tag(), "Skipping song failed", it)
+              Toast.makeText(context, R.string.skip_error, Toast.LENGTH_SHORT).show()
+              if (it is ApiException && it.code == 403) Auth.clear()
+              close(view)
+            }) //TODO store?
+      }
+
+      private fun close(view: SwipeRevealLayout) {
+        skip_progress.clearAnimation()
+        skip_progress.isVisible = false
+        skip_text.isVisible = true
+        view.close(true)
+        view.setLockDrag(false)
+      }
+    })
+  }
+
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
-    val view = view ?: throw IllegalStateException()
     song_title.isSelected = true
     song_description.isSelected = true
     song_queuer.isSelected = true
-    view.setOnTouchListener(NextSwipeListener(context))
   }
 
   override fun onStart() {
     super.onStart()
     initObservers()
+
+    Observable.interval(1, TimeUnit.SECONDS)
+        .subscribeOn(Schedulers.io())
+        .map { Auth.hasPermissionNoRefresh(Permission.SKIP) }
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe({
+          root.setLockDrag(!it)
+          if (!it) root.close(false)
+        }, {})
+        .store()
   }
 
   override fun onStop() {
@@ -104,7 +147,7 @@ class PlayerFragment : Fragment(), ObserverUser {
         .store()
   }
 
-  private operator fun next() {
+  private fun next() {
     Single.fromCallable { Auth.hasPermission(Permission.SKIP) }
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
@@ -200,105 +243,5 @@ class PlayerFragment : Fragment(), ObserverUser {
         .load(albumArtUrl)
         .placeholder(R.drawable.ic_broken_image)
         .into(albumArt)
-  }
-
-  private inner class NextSwipeListener(ctx: Context) : OnTouchListener, OnGestureListener {
-
-    private val gestureDetector: GestureDetectorCompat = GestureDetectorCompat(ctx, this)
-
-    /**
-     * Called when a touch event is dispatched to a view. This allows listeners to
-     * get a chance to respond before the target view.
-     *
-     * @param v The view the touch event has been dispatched to.
-     * @param event The MotionEvent object containing full information about the event.
-     * @return True if the listener has consumed the event, false otherwise.
-     */
-    override fun onTouch(v: View, event: MotionEvent): Boolean =
-        gestureDetector.onTouchEvent(event)
-
-    /**
-     * Notified when a tap occurs with the down [MotionEvent]
-     * that triggered it. This will be triggered immediately for
-     * every down event. All other events should be preceded by this.
-     *
-     * @param e The down motion event.
-     */
-    override fun onDown(e: MotionEvent): Boolean = true
-
-    /**
-     * The userName has performed a down [MotionEvent] and not performed
-     * a move or up yet. This event is commonly used to provide visual
-     * feedback to the userName to let them know that their action has been
-     * recognized i.e. highlight an element.
-     *
-     * @param e The down motion event
-     */
-    override fun onShowPress(e: MotionEvent) {}
-
-    /**
-     * Notified when a tap occurs with the up [MotionEvent]
-     * that triggered it.
-     *
-     * @param e The up motion event that completed the first tap
-     * @return true if the event is consumed, else false
-     */
-    override fun onSingleTapUp(e: MotionEvent): Boolean = true
-
-    /**
-     * Notified when a scroll occurs with the initial on down [MotionEvent] and the
-     * current move [MotionEvent]. The distance in x and y is also supplied for
-     * convenience.
-     *
-     * @param e1 The first down motion event that started the scrolling.
-     * @param e2 The move motion event that triggered the current onScroll.
-     * @param distanceX The distance along the X axis that has been scrolled since the last call to
-     * onScroll. This is NOT the distance between `e1` and `e2`.
-     * @param distanceY The distance along the Y axis that has been scrolled since the last call to
-     * onScroll. This is NOT the distance between `e1` and `e2`.
-     * @return true if the event is consumed, else false
-     */
-    override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float,
-        distanceY: Float): Boolean =
-        true
-
-    /**
-     * Notified when a long press occurs with the initial on down [MotionEvent]
-     * that trigged it.
-     *
-     * @param e The initial on down motion event that started the longpress.
-     */
-    override fun onLongPress(e: MotionEvent) {}
-
-    /**
-     * Notified of a fling event when it occurs with the initial on down [MotionEvent]
-     * and the matching up [MotionEvent]. The calculated velocity is supplied along
-     * the x and y axis in pixels per second.
-     *
-     * @param e1 The first down motion event that started the fling.
-     * @param e2 The move motion event that triggered the current onFling.
-     * @param velocityX The velocity of this fling measured in pixels per second along the x axis.
-     * @param velocityY The velocity of this fling measured in pixels per second along the y axis.
-     * @return true if the event is consumed, else false
-     */
-    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float,
-        velocityY: Float): Boolean {
-      var result = false
-      val diffY = e2.y - e1.y
-      val diffX = e2.x - e1.x
-      if (Math.abs(diffX) > Math.abs(diffY)) {
-        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-          if (diffX < 0) {
-            onSwipeLeft()
-          }
-          result = true
-        }
-      }
-      return result
-    }
-
-    private fun onSwipeLeft() {
-      next()
-    }
   }
 }
