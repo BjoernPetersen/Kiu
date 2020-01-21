5a03608d4a8b9eaf6e1cb7471f8ab3a007389af8
Björn Petersen <pheasn@gmail.com>
2020-01-21 18:10:36 +0100

    Properly update suggestion list on removal

---
+++ lib/view/widget/suggestion_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:kiu/bot/connection_manager.dart';
+import 'package:kiu/bot/model.dart';
+import 'package:kiu/bot/permission.dart';
+import 'package:kiu/data/dependency_model.dart';
+import 'package:kiu/view/widget/result_list.dart';
+
+class SuggestionList extends StatefulWidget {
+  final NamedPlugin suggester;
+  final List<Song> suggestions;
+
+  const SuggestionList({
+    Key key,
+    @required this.suggester,
+    @required this.suggestions,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() =>
+      _SuggestionListState(suggester, suggestions);
+}
+
+class _SuggestionListState extends State<SuggestionList> {
+  final NamedPlugin suggester;
+  List<Song> suggestions;
+
+  _SuggestionListState(this.suggester, this.suggestions);
+
+  Future<void> _delete(ConnectionManager manager, Song song) async {
+    final bot = await manager.getService();
+    await bot.removeSuggestion(
+        suggesterId: suggester.id,
+        providerId: song.provider.id,
+        songId: song.id);
+    setState(() {
+      suggestions.remove(song);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final manager = service<ConnectionManager>();
+    if (manager.hasPermission(Permission.DISLIKE)) {
+      return ResultList(
+        results: suggestions,
+        trailingBuilder: (_, song) {
+          return IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () => _delete(manager, song),
+          );
+        },
+      );
+    } else {
+      return ResultList(results: suggestions);
+    }
+  }
+}
--- lib/view/widget/suggestions_content.dart
+++ lib/view/widget/suggestions_content.dart
@@ -1,51 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:kiu/bot/connection_manager.dart';
 import 'package:kiu/bot/model.dart';
-import 'package:kiu/bot/permission.dart';
 import 'package:kiu/data/dependency_model.dart';
 import 'package:kiu/view/widget/loading_delegate.dart';
-import 'package:kiu/view/widget/result_list.dart';
+import 'package:kiu/view/widget/suggestion_list.dart';
 
 class SuggestionsContent extends StatelessWidget {
   final NamedPlugin suggester;
 
   const SuggestionsContent(this.suggester) : super();
 
-  Future<List<Song>> Function() _retrieve(ConnectionManager manager) {
-    return () async {
-      final bot = await manager.getService();
-      return bot.getSuggestions(suggester.id);
-    };
-  }
-
-  Future<void> _delete(ConnectionManager manager, Song song) async {
-    final bot = await manager.getService();
-    await bot.removeSuggestion(
-        suggesterId: suggester.id,
-        providerId: song.provider.id,
-        songId: song.id);
-    // TODO would be nice to remove from list here
+  Future<List<Song>> _retrieve() async {
+    final bot = await service<ConnectionManager>().getService();
+    return bot.getSuggestions(suggester.id);
   }
 
   @override
   Widget build(BuildContext context) {
-    final manager = service<ConnectionManager>();
-    if (manager.hasPermission(Permission.DISLIKE)) {
-      return LoadingDelegate(
-          action: _retrieve(manager),
-          itemBuilder: (_, result) => ResultList(
-                results: result,
-                trailingBuilder: (_, song) {
-                  return IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _delete(manager, song),
-                  );
-                },
-              ));
-    } else {
-      return LoadingDelegate(
-          action: _retrieve(manager),
-          itemBuilder: (_, result) => ResultList(results: result));
-    }
+    return LoadingDelegate(
+        action: _retrieve,
+        itemBuilder: (_, result) => SuggestionList(
+              suggester: suggester,
+              suggestions: result,
+            ));
   }
 }
