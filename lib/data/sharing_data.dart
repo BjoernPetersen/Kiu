755835e15e5905ef646355328930134b40d3d6d6
Björn Petersen <pheasn@gmail.com>
2020-01-24 20:01:55 +0100

    Add share option (Closes #53)

---
+++ lib/data/sharing_data.dart
@@ -0,0 +1,48 @@
+class SharingData {
+  final String providerId;
+  final String songId;
+
+  const SharingData({this.providerId, this.songId});
+
+  @override
+  String toString() {
+    return 'SharingData{providerId: $providerId, songId: $songId}';
+  }
+}
+
+const String YOUTUBE_ID =
+    'net.bjoernpetersen.musicbot.spi.plugin.predefined.youtube.YouTubeProvider';
+const String SPOTIFY_ID =
+    'net.bjoernpetersen.musicbot.spi.plugin.predefined.spotify.SpotifyProvider';
+
+SharingData _extractYoutubeLong(Uri uri) {
+  String songId = uri.queryParameters['v'];
+  if (songId?.isNotEmpty == true) {
+    return SharingData(providerId: YOUTUBE_ID, songId: songId);
+  } else {
+    return null;
+  }
+}
+
+SharingData _extractYoutubeShort(Uri uri) {
+  return SharingData(providerId: YOUTUBE_ID, songId: uri.pathSegments.first);
+}
+
+SharingData _extractSpotify(Uri uri) {
+  return SharingData(providerId: SPOTIFY_ID, songId: uri.pathSegments.first);
+}
+
+SharingData extractSharingData(String url) {
+  final uri = Uri.tryParse(url);
+  if (uri != null) {
+    switch (uri.host) {
+      case "youtu.be":
+        return _extractYoutubeShort(uri);
+      case "youtube.com":
+        return _extractYoutubeLong(uri);
+      case "open.spotify.com":
+        return _extractSpotify(uri);
+    }
+  }
+  return null;
+}
--- lib/view/page/queue_page.dart
+++ lib/view/page/queue_page.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kiu/bot/connection_manager.dart';
+import 'package:kiu/data/dependency_model.dart';
+import 'package:kiu/data/sharing_data.dart';
 import 'package:kiu/view/page/overflow.dart';
 import 'package:kiu/view/widget/embedded_player.dart';
 import 'package:kiu/view/widget/navigation_bar.dart';
 import 'package:kiu/view/widget/queue_list.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-class QueuePage extends StatelessWidget {
+class QueuePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text('Kiu'),
-        actions: <Widget>[createOverflowItems(context)],
-      ),
-      body: EmbeddedPlayer(child: QueueList()),
-      bottomNavigationBar: NavigationBar(BottomCategory.queue));
+  _QueuePageState createState() => _QueuePageState();
+}
+
+class _QueuePageState extends State<QueuePage> {
+  bool _isHandlingShares = false;
+
+  Future<void> _handleShare(String share) async {
+    if (share == null) return;
+    final parsed = extractSharingData(share);
+    if (parsed == null) return;
+    try {
+      final bot = await service<ConnectionManager>().getService();
+      await bot.enqueue(parsed.songId, parsed.providerId);
+    } catch (err) {
+      await Fluttertoast.showToast(msg: "Could not enqueue shared song");
+    }
+  }
+
+  _handleShares() {
+    _isHandlingShares = true;
+    ReceiveSharingIntent.getInitialText().then(_handleShare);
+    ReceiveSharingIntent.getTextStream().listen(_handleShare);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isHandlingShares) {
+      _handleShares();
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Kiu'),
+          actions: <Widget>[createOverflowItems(context)],
+        ),
+        body: EmbeddedPlayer(child: QueueList()),
+        bottomNavigationBar: NavigationBar(BottomCategory.queue));
+  }
 }
