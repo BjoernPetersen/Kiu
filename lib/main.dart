023d5b75f17f36e423dc6f45665a8e795d8a2b46
Björn Petersen <pheasn@gmail.com>
2020-02-20 17:03:03 +0100

    Remove flutter comment

--- lib/main.dart
+++ lib/main.dart
@@ -65,15 +65,6 @@ class _KiuState extends State<Kiu> {
     return MaterialApp(
       title: 'Kiu',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Color(0xFFBBC0CA),
         highlightColor: Color(0xFFB0B5C1),
       ),
